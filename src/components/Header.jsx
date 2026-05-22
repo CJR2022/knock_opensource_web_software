22956d0b1edd50283a5378a85cb6012e4df409fa
CJR <wjdfbs13579@gmail.com>
2026-05-22 23:18:54 +0900

    헤더 링크방식 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,4 @@
-import { useLocation } from "react-router-dom"; //현 URL 정보 가져오는 친구
-
-//헤더 메뉴탭도 NavLink 방식으로 바꿀검다
+import { NavLink} from "react-router-dom";
 
 const tabs = [
   { id: "main", label: "메인", href: "/" },
@@ -9,11 +7,6 @@ const tabs = [
 ];
 
 export default function Header() {
-  const location = useLocation(); //URL 정보 가져오기
-  const pathname = location.pathname;//슬래시~ 내용 ex)"/admin"
-  const activeTab = pathname === "/" ? "main" :
-                   pathname === "/inquiry" ? "inquiry" : "admin";
-
   return (
     <header className="header">
       <div className="header-inner">
@@ -27,13 +20,13 @@ export default function Header() {
 
           <nav className="nav-pill">
             {tabs.map((tab) => (
-              <a
+              <NavLink
                 key={tab.id}
-                href={tab.href}
-                className={`nav-tab ${activeTab === tab.id ? "active" : ""}`}
+                to={tab.href}
+                className={({isActive})=>isActive?"nav-tab active":"nav-tab"}
               >
                 {tab.label}
-              </a>
+              </NavLink>
             ))}
           </nav>
         </div>
