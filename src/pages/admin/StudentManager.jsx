a04f5fa95b162aa8017b8d84fdefddb538100c00
CJR <wjdfbs13579@gmail.com>
2026-05-26 02:46:30 +0900

    검색바 레이아웃

--- src/pages/admin/StudentManager.jsx
+++ src/pages/admin/StudentManager.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import "./student.css";
+import searchIcon from "../../assets/icons/search.svg";
 
 export default function StudentManager() {
   const [pendingStudents, setPendingStudents] = useState([]);
@@ -21,11 +23,15 @@ export default function StudentManager() {
   return (
     <div className="page">
       <div className="flex flex-col gap-6">
+        <label className="searchBar p-4">
+          <img src={searchIcon} alt="검색" className="w-4 h-4 mr-2 mb-1"/>
+          <input type="text" className="search flex-1" placeholder="학번 혹은 이름을 입력하여 검색" ></input>
+        </label>
         <section>
           <h2 className="text-lg font-bold mb-3">신규 가입자</h2>
           <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
             {pendingStudents.map((student) => (
-              <div key={student.id} className="card p-4">
+              <div key={student.id} className="card newStudent p-4">
                 <p>{student.student_number}</p>
                 <p>{student.name}</p>
                 <p>{student.phone}</p>
